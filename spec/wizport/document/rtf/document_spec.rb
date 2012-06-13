94f9198c6bd5f70bddf3af0dfb421bd02f973f3e
songgz <sgzhe@163.com>
2012-06-13 10:37:06 +0800

    modify rtf table

--- spec/wizport/document/rtf/document_spec.rb
+++ spec/wizport/document/rtf/document_spec.rb
@@ -7,7 +7,7 @@ describe Wizport::Rtf::Document do
   it "a simple example of Rtf" do
       rtf = Wizport::Rtf::Document.new do
         text "学生综合素质评价"
-        table [[1,{content:'1',colspan:2}],[3,{content:'4',rowspan:2,colspan:2}],[11]] do
+        table [[1,{content:'1',colspan:2}],[3,{content:'4',rowspan:2,colspan:2}],[11]],column_widths:{1=>2000,2=>1000,3=>1000} do
           add_row [1,1,1]
         end
       end
